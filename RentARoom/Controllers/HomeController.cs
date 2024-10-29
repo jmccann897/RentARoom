3c1754d64d592ca20ab3272fe54481941cb26978
Josh <55717114+jmccann897@users.noreply.github.com>
2024-10-29 20:52:05 +0000

    landing page UI update
    landing page UI update

--- RentARoom/Controllers/HomeController.cs
+++ RentARoom/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using RentARoom.DataAccess.Repository.IRepository;
 using RentARoom.Models;
 using System.Diagnostics;
 
@@ -7,15 +8,24 @@ namespace RentARoom.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Property> propertyList = _unitOfWork.Property.GetAll(includeProperties: "PropertyType");
+            return View(propertyList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Property property = _unitOfWork.Property.Get(u => u.Id == id,includeProperties: "PropertyType");
+            return View(property);
         }
 
         public IActionResult Privacy()
