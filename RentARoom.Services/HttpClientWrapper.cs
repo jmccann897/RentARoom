edf664eaa360431ecd1573a2df0fa12f69515fd4
Josh <55717114+jmccann897@users.noreply.github.com>
2025-03-21 08:04:05 +0000

    Add travel time service unit tests
    Add travel time service unit tests
    Add http client wrapper

---
+++ RentARoom.Services/HttpClientWrapper.cs
@@ -0,0 +1,41 @@
+﻿using RentARoom.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentARoom.Services.IServices
+{
+    public class HttpClientWrapper : IHttpClientWrapper
+    {
+        private readonly HttpClient _httpClient;
+        public HttpClientWrapper() 
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.openrouteservice.org")
+            };
+
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+        }
+        public HttpClientWrapper(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public HttpRequestHeaders DefaultRequestHeaders => _httpClient.DefaultRequestHeaders;
+
+        public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content)
+        {
+            return _httpClient.PostAsync(requestUri, content);
+        }
+
+        public Task<HttpResponseMessage> GetAsync(string requestUri)
+        {
+            return _httpClient.GetAsync(requestUri);
+        }
+    }
+}
